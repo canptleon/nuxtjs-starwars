2763a29e02af0284f8cc10359ea93553289774f7
Arda Keyişoğlu <ardakeyisoglu@gmail.com>
2024-09-21 12:40:24 +0300

    mobile, video clicking bug fix.

--- components/BackgroundVideo.vue
+++ components/BackgroundVideo.vue
@@ -1,5 +1,5 @@
 <template>
-  <video class="background-video" autoplay muted loop>
+  <video class="background-video" autoplay muted loop playsinline>
     <source src="/bg-video.mp4" type="video/mp4" />
   </video>
 </template>
